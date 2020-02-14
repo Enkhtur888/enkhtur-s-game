90f0469bcc46b743d229f65a47bf480733d49fe8
enkhtur <enkhtur.undrakh596@yahoo.com>
2020-02-14 15:41:43 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -8,21 +8,18 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Shoonii ali talaaraa buusniiig hadgalah huvisagch heregtei, 1-6 gesen utgiig ene huvisagchid sanamsarguigeer uusgej ugnu.
-var dice = Math.floor(Math.random() * 6) + 1;
-
-// <div class="player-score" id="score-0">43</div>
-// window.document.querySelector("#score-0").textContent = dice;
-
-// document.querySelector("#score-1").textContent = ;
 
 // Programm ehlehed beltgey
+document.getElementById("score-0"). textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-
-document.querySelector(".dice").style.display = "none";
-
-console.log('shoo :' + dice);
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+});
